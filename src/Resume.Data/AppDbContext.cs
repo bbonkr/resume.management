74b97501c187d49bb3ad1a0c54a276d1a488074b
Pon Cheol Ku (구본철) <bbon@live.com>
2022-05-23 16:52:49 +0900

    Upddate DbContext

--- src/Resume.Data/AppDbContext.cs
+++ src/Resume.Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
+using Resume.Entities;
 
 namespace Resume.Data
 {
@@ -7,6 +8,33 @@ namespace Resume.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
         {
+        }
+        
+        public DbSet<User> Users { get; set; }
+        
+        public DbSet<Content> Contents { get; set; }
+        
+        public DbSet<ContentLink> ContentLinks { get; set; }
+        
+        public DbSet<ContentMedia> ContentMedias { get; set; }
+        
+        public DbSet<ContentTag> ContentTags { get; set; }
+        
+        public DbSet<Skill> Skills { get; set; }
+        
+        public DbSet<SkillGroup> SkillGroups { get; set; }
+        
+        public DbSet<Tag> Tags { get; set; }
+        
+        public DbSet<UserLink> UserLinks { get; set; }
+        
+        public DbSet<UserMedia> UserMedias { get; set; }
+        
+        public DbSet<UserSns> UserSns { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
         }
     }
 }
